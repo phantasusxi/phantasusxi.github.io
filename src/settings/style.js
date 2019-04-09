89e75adb624dc78fa0b7272fd3d1f93ae021b6ae
phantasusXI <47456932+phantasusxi@users.noreply.github.com>
2019-04-09 18:45:25 -0300

    feat: add base components with new style

---
+++ src/settings/style.js
@@ -0,0 +1,19 @@
+// ..........................
+
+export const colors = {
+  main: '#8937df',
+  dark: '#592088',
+  selection: '#8e44ad',
+  secondary: '#fff',
+}
+
+// ..........................
+
+export const type = {
+  family: `'Avenir Next', -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif`,
+  size: {
+    root: '16px'
+  }
+}
+
+// ..........................
